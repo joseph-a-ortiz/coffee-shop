d3d1759e4c1834325d30fd0ba6b41dd082f9dc5c
ortizjoseph7 <ortizjoseph7@gmail.com>
2021-09-03 16:04:28 -0500

    code

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 
-    $(function () {
+ $(document).ready(function () {
     $(window).on('scroll', function () {
         if ( $(window).scrollTop() > 10 ) {
             $('.navbar').addClass('active');
