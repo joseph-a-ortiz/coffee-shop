96ad2ea0e4aa6bba68dc45d8df304be252aaf2e1
ortizjoseph7 <ortizjoseph7@gmail.com>
2021-09-04 11:14:30 -0500

    navbar issued fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,11 @@
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
 
- 
-    $(window).scroll(function() {
-        if ( $(window).scrollTop() > 10 ) {
-            $('.navbar').addClass('active');
-        } else {
-            $('.navbar').removeClass('active');
-        }
-    });
-
-var docWidth = document.documentElement.offsetWidth;
-
-[].forEach.call(
-  document.querySelectorAll('*'),
-  function(el) {
-    if (el.offsetWidth > docWidth) {
-      console.log(el);
-    }
+  // 20 is an arbitrary number here, just to make you think if you need the prevScrollpos variable:
+  if (currentScrollPos > 30) {
+    // I am using 'display' instead of 'top':
+    document.getElementById("navbar2").style.display = "initial";
+  } else {
+    document.getElementById("navbar2").style.display = "none";
   }
-);
-
+}
